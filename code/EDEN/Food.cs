7feb8ca788f826e3e801f3af433173b4219e9cc6
Sean <sean.brereton4@mail.dcu.ie>
2020-02-07 13:02:10 +0000

    updated food class

--- code/EDEN/Food.cs
+++ code/EDEN/Food.cs
@@ -1,8 +1,18 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 namespace EDEN {
     public class Food : Entity {
-        public Food()
-        {
+        public Food() {
+
+            position = Rand.Range(new Vector2(
+                Application.graphics.PreferredBackBufferWidth,
+                Application.graphics.PreferredBackBufferHeight)
+            );
+
+            Color color = Color.Beige;
+            int height = 5;
+            int width = 5;
+            texture = Textures.Rect(color, height, width);
         }
     }
 }
