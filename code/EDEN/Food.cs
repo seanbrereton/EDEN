b5924907192743f12c7f29bcfe455de81e51dd51
Sean <sean.brereton4@mail.dcu.ie>
2020-02-07 14:01:21 +0000

    added comments to food.cs and updated eden.csproj

--- code/EDEN/Food.cs
+++ code/EDEN/Food.cs
@@ -3,16 +3,18 @@ using System;
 namespace EDEN {
     public class Food : Entity {
         public Food() {
+            //Initialise food values
+            Color color = Color.Beige;
+            int height = 5;
+            int width = 5;
+            //Draws food
+            texture = Textures.Rect(color, height, width);
 
+            //Chooses random position to spawn
             position = Rand.Range(new Vector2(
                 Application.graphics.PreferredBackBufferWidth,
                 Application.graphics.PreferredBackBufferHeight)
             );
-
-            Color color = Color.Beige;
-            int height = 5;
-            int width = 5;
-            texture = Textures.Rect(color, height, width);
         }
     }
 }
