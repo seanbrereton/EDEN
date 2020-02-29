dc65b9a85c03fc58e91488117ca101b3678d5279
Sean <sean.brereton4@mail.dcu.ie>
2020-02-29 16:27:52 +0000

    implemented main menu

--- code/EDEN/Button.cs
+++ code/EDEN/Button.cs
@@ -3,9 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace EDEN {
-    class Button : Component {
+    class Button : Entity {
+
+        public string text;
+        public Color fontColor = Color.Black;
+        public Color hoverColor = Color.Gray;
+        public Color defaultColor;
+        public Action action;
+
+        public Button(int width, int height, Color col, Vector2 pos, String _text, Action _action) : base(pos) {
+            texture = Textures.Rect(Color.White, width, height);
+            defaultColor = col;
+            text = _text;
+            action = _action;
+        }
+
+        public bool isPressed() {
+            return Input.Click(0, true) && rect.Contains(Input.MousePos);   
+        }
+
+        public bool isHover() {
+            return rect.Contains(Input.MousePos);   
+        }
+        
+        public override void HandleInput() {
+            if (isHover()) {
+                color = hoverColor;
+            } else {
+                color = defaultColor;
+            }
+
+            if (isPressed())
+                action();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if(!string.IsNullOrEmpty(text)) {
+                float x = (rect.X + (rect.Width / 2)) - (Application.font.MeasureString(text).X / 2);
+                float y = (rect.Y + (rect.Height / 2)) - (Application.font.MeasureString(text).Y / 2);
+
+                spriteBatch.DrawString(Application.font, text, new Vector2(x, y), fontColor);
+
+            }
+        }
 
     }
 }
